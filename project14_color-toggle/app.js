dc6826064a8a1635ee84c89d5689d272d6a9f58b
Deepak Lodha <123529334+deepaklodha532@users.noreply.github.com>
2024-03-05 08:56:55 +0000

    project14

---
+++ project14_color-toggle/app.js
@@ -0,0 +1,11 @@
+let toggler  =  document.getElementById("switch") ;
+toggler.addEventListener ('click',()=>{
+    //  console.log(toggler.checked) ; 
+    if(toggler.checked ===true ) {
+        document.body.style.backgroundColor = 'black' ;
+    }
+    else{
+        document.body.style.backgroundColor = "white" ; 
+
+    }
+});
