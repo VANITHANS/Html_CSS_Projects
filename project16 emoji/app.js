9672d08a5ff7d756eafc2ecaf16b832003cd4efa
Deepak Lodha <123529334+deepaklodha532@users.noreply.github.com>
2024-03-05 10:05:52 +0000

    project17

---
+++ project16 emoji/app.js
@@ -0,0 +1,10 @@
+const btn =  document.querySelector(".emoji");
+const emojis =  
+[
+
+    "🤣","😍","🌹","😜","🤞","😘","❤️","😂","😊","💕","😉","😉","😗","😗","🤗","😪","😶‍🌫️","😭","😧","😰","😬","😩","🥵","😠","🥶","😳","🤮"
+];
+
+btn.addEventListener("mouseover",()=>{
+    btn.innerHTML = emojis[Math.floor(Math.random()*emojis.length)] ;
+})
