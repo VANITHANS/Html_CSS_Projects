e91b63cecd5d5e4464b57a6a5b67612864f5a48b
Deepak Lodha <123529334+deepaklodha532@users.noreply.github.com>
2024-03-07 06:00:30 +0000

    project21

---
+++ project21 Dynamic Form  Popup/app.js
@@ -0,0 +1,14 @@
+// close btn 
+const close  =  document.getElementById("close") ; 
+// open btn 
+const  open =  document.getElementById ("open" );
+// modal 
+const modal =  document.getElementById("modal"); 
+
+open.addEventListener("click", ()=> modal.classList.add("show-modal")); 
+close.addEventListener ("click", ()=>modal.classList.remove("show-modal"));
+
+window.addEventListener ('click' , e=> {
+
+    e.target === modal ? modal.classList.remove ('show-modal' ):false
+});
